a0da22b96f88b18b4a879a9398d581f2480a6179
Dan Gowans <dangowans@users.noreply.github.com>
2026-08-13 11:22:16 -0400

    limit splits

--- utilities.ts
+++ utilities.ts
@@ -6,12 +6,13 @@
 export function getUsernamePart(username: string): string {
   if (username.includes('@')) {
     // If the user name includes an '@', split by '@' and return the first part.
-    const parts = username.split('@')
+    const parts = username.split('@', 1)
     return parts[0]
   }
 
   // Extract the user name part from a user name that may include a domain.
-  const parts = username.split('\\')
+  // eslint-disable-next-line @typescript-eslint/no-magic-numbers
+  const parts = username.split('\\', 3)
 
   const usernamePart = parts.length === 2 ? parts[1] : parts[0]
 
